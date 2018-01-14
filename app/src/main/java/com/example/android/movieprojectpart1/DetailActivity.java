1e7c1c490e97b5d1e935a901544fdcba1127099a
john-sawicki <3Gxapnil>
2018-01-14 15:15:49 -0600

    detail view populates based on single array from main

--- app/src/main/java/com/example/android/movieprojectpart1/DetailActivity.java
+++ app/src/main/java/com/example/android/movieprojectpart1/DetailActivity.java
@@ -6,19 +6,32 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 public class DetailActivity extends AppCompatActivity {
     TextView titleText, releaseDateText, userRatingText, overviewText;
-    ImageView moviePoster;
-    String movieData;
+    ImageView moviePosterImage;
+    String[] movieData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        moviePosterImage = findViewById(R.id.moviePoster);
         titleText = findViewById(R.id.movieTitle);
-        releaseDateText = findViewById(R.id.releaseTitle);
+        releaseDateText = findViewById(R.id.movieReleased);
+        userRatingText = findViewById(R.id.movieVote);
+        overviewText = findViewById(R.id.movieOverview);
+
         Intent intent = getIntent();
         if(intent.hasExtra("detail data")==true){
-            movieData= intent.getStringExtra("detail data");
+            movieData= intent.getStringArrayExtra("detail data");
+            Picasso.with(getApplicationContext())
+                    .load("https://image.tmdb.org/t/p/w185"+movieData[0])
+                    .into(moviePosterImage);
+            titleText.setText(movieData[1]);
+            releaseDateText.setText(movieData[2]);
+            userRatingText.setText(movieData[3]);
+            overviewText.setText(movieData[4]);
         }
     }
 }
