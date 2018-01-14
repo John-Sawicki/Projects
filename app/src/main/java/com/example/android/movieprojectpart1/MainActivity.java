927b83673cc080710d082b980b8936b977f3546e
john-sawicki <3Gxapnil>
2018-01-13 21:02:13 -0600

    added menu

--- app/src/main/java/com/example/android/movieprojectpart1/MainActivity.java
+++ app/src/main/java/com/example/android/movieprojectpart1/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.android.movieprojectpart1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    String searchMethod;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemClicked = item.getItemId();
+        if(menuItemClicked==R.id.menu_popular)
+            searchMethod="popular/";
+        if(menuItemClicked==R.id.menu_rating)
+            searchMethod="top_rated/";
+        return true;
     }
 }
